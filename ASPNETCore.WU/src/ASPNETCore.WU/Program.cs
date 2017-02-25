11b80a97709b299279cfc14b6c2959de16f0ecf9
gplesz <gabor.plesz@gmail.com>
2017-02-25 14:56:53 +0100

    A figyelő url-t beállítottuk

--- ASPNETCore.WU/src/ASPNETCore.WU/Program.cs
+++ ASPNETCore.WU/src/ASPNETCore.WU/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace ASPNETCore.WU
 {
@@ -11,7 +12,16 @@ namespace ASPNETCore.WU
     {
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("hosting.json", optional: true)
+                    .AddEnvironmentVariables(prefix: "ASPNETCORE_")
+                    .AddCommandLine(args)
+                    .Build();
+
             var host = new WebHostBuilder()
+                .UseUrls("http://*:1000", "https://*:1234", "http://0.0.0.0:5000")
+                .UseConfiguration(config)
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
